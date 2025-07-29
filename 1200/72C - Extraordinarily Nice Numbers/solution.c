35751fbeba10664b8d3ee0c73d83d937e75f3ec3
Md. Mahir Uddin <157863988+Mahir-Uddin0@users.noreply.github.com>
2025-07-30 00:22:19 +0600

    Update solution.c

--- 1200/72C - Extraordinarily Nice Numbers/solution.c
+++ 1200/72C - Extraordinarily Nice Numbers/solution.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int x;
+    scanf("%d", &x);
+
+    int odd = 0, even = 0;
+
+    for (int i = 1; i <= x / 2; i++) {
+        if (x % i == 0) {
+            if (i % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+    }
+
+    if (x % 2 == 0)
+        even++;
+    else
+        odd++;
+
+    if (odd == even)
+        printf("YES\n");
+    else
+        printf("NO\n");
+
+    return 0;
+}
 
