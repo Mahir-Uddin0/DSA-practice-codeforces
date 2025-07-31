c550cabf4a7971d14bf74969b93baa5f612f3e38
Md. Mahir Uddin <157863988+Mahir-Uddin0@users.noreply.github.com>
2025-07-31 10:57:42 +0600

    Update solution.c

--- 1200/A - Cheap Travel/solution.c
+++ 1200/A - Cheap Travel/solution.c
@@ -1,1 +1,13 @@
+#include<stdio.h>
 
+int main(){
+    int total_ride,m_ride,ticket_price,m_ride_price;
+    scanf("%d %d %d %d",&total_ride,&m_ride,&ticket_price,&m_ride_price);
+    
+    if(m_ride_price/m_ride>=ticket_price) printf("%d",total_ride*ticket_price);
+    else{
+        if(total_ride%m_ride*ticket_price<m_ride_price) printf("%d",total_ride/m_ride*m_ride_price+total_ride%m_ride*ticket_price);
+        else printf("%d",(total_ride/m_ride+1)*m_ride_price);
+    }
+    return 0;
+}
