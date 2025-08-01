89c0a224a912991100ae494cb886a755a521fa62
Md. Mahir Uddin <157863988+Mahir-Uddin0@users.noreply.github.com>
2025-08-01 22:03:10 +0600

    Update solution.c

--- 1200/C - K-th Not Divisible by n/solution.c
+++ 1200/C - K-th Not Divisible by n/solution.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
 
+int main(){
+    int t;
+    scanf("%d",&t);
+    int n[t],k[t];
+    for(int i=0; i<t; i++) scanf("%d %d",&n[i],&k[i]);
+    
+    for(int i=0; i<t; i++){
+        if(k[i]%(n[i]-1)==0) printf("%d\n",k[i]/(n[i]-1)*n[i]+k[i]%(n[i]-1)-1);
+        else printf("%d\n",k[i]/(n[i]-1)*n[i]+k[i]%(n[i]-1));
+    }
+    return 0;
+}
