a673d5a69655f48e010685603ef3b0f7726d343b
Md. Mahir Uddin <157863988+Mahir-Uddin0@users.noreply.github.com>
2025-08-06 23:37:12 +0600

    Update solution.c

--- 1200/A - Chewbaсca and Number/solution.c
+++ 1200/A - Chewbaсca and Number/solution.c
@@ -1,1 +1,21 @@
-
+#include<stdio.h>
+long long power(int base, int exp){
+    long long result=1;
+    if(exp==0) return 1;
+    for(int i=1; i<=exp; i++){
+        result*=10;
+    }
+    return result;
+}
+int main(){
+    long long x,y=0;
+    scanf("%lld",&x);
+    for(int i=0; x!=0; i++){
+        if(x/10==0 && x==9) y = y + x*(long long)power(10,i);
+        if(x%10>4) y+= (long long)power(10,i)*(9-x%10);
+        else y+= x%10*(long long)power(10,i);
+        x = x/10;
+    }
+    printf("%lld",y);
+    return 0;
+}
