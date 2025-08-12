597ed7b2d87095d4a20c7c1f1d8c28074510e80f
Md. Mahir Uddin <157863988+Mahir-Uddin0@users.noreply.github.com>
2025-08-12 23:53:21 +0600

    Update solution.c

--- 1200/A - DZY Loves Chessboard/solution.c
+++ 1200/A - DZY Loves Chessboard/solution.c
@@ -1,1 +1,33 @@
 
+#include <stdio.h>
+
+int main() {
+    int n,m;
+    scanf("%d %d",&n,&m);
+    getchar();
+    char board[n][m];
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            scanf("%c",&board[i][j]);
+        }
+        getchar();
+    }
+    
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            if(board[i][j]=='.'){
+                if((i+j)%2==0) board[i][j] = 'B';
+                else board[i][j] = 'W';
+            }
+        }
+    }
+    
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            printf("%c",board[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
