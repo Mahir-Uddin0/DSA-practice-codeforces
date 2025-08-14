340ba505edbce144209e74c14ed715fa167fac60
Md. Mahir Uddin <157863988+Mahir-Uddin0@users.noreply.github.com>
2025-08-15 00:18:31 +0600

    Update solution.c

--- 1200/A. Odd Selection/solution.c
+++ 1200/A. Odd Selection/solution.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
 
+int main() {
+    int t,temp,n,x,odd_count,even_count;
+    scanf("%d",&t);
+    for(int i=0; i<t; i++){
+        scanf("%d %d",&n,&x);
+        odd_count=0, even_count=0;
+        for(int j=0; j<n; j++){
+            scanf("%d",&temp);
+            if(temp%2==0) even_count++;
+            else odd_count++;
+        }
+        if (x<=even_count){
+            if (odd_count>0) printf("YES\n");
+            else printf("NO\n");
+        }
+        else{
+            if ((x-even_count)%2!=0) printf("YES\n");
+            else{
+                if ((x-even_count)<odd_count && even_count>0) printf("YES\n");
+                else printf("NO\n");
+            }
+        }
+    }
+    return 0;
+}
