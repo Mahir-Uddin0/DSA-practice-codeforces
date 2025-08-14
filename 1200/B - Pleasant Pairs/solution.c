f9069d2d88128007f615b433c50b73d74fe0d04c
Md. Mahir Uddin <157863988+Mahir-Uddin0@users.noreply.github.com>
2025-08-14 23:58:54 +0600

    Update solution.c

--- 1200/B - Pleasant Pairs/solution.c
+++ 1200/B - Pleasant Pairs/solution.c
@@ -1,1 +1,22 @@
+#include<stdio.h>
 
+int main(){
+    int t,n;
+    scanf("%d",&t);
+    
+    for(int k=0; k<t; k++){
+        scanf("%d",&n);
+        int a[n];
+        for(int i=0; i<n; i++) scanf("%d",&a[i]);
+        int count = 0;
+        
+        for(int i=0; i<n; i++){
+            for(int j=(a[i]+i-(2*(i+1))%a[i]), l=(i+j+2)/a[i]; j<n; j+=a[i],l++)
+                if(a[j]==l) count++;
+        }
+        printf("%d\n",count);
+    }
+    
+    
+    return 0;
+}
