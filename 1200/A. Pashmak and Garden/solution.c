72a1d9157c589e49dcdf3788f32df938f0e3b9f2
Md. Mahir Uddin <157863988+Mahir-Uddin0@users.noreply.github.com>
2025-09-23 23:55:38 +0600

    Update solution.c

--- 1200/A. Pashmak and Garden/solution.c
+++ 1200/A. Pashmak and Garden/solution.c
@@ -1,1 +1,27 @@
+#include<stdio.h>
 
+int main(){
+    int x1, y1, x2, y2, x3, y3, x4, y4;
+    scanf("%d %d %d %d",&x1,&y1,&x2,&y2);
+    
+    if(x1!=x2 && y1!=y2){
+        if((x1-x2)*(x1-x2) != (y1-y2)*(y1-y2)){
+            printf("-1");
+            return 0;
+        }
+        else{
+            printf("%d %d %d %d",x1,y2,x2,y1);
+        }
+    }
+    else if(y1==y2){
+        int length = x1-x2;
+        printf("%d %d %d %d",x1,y1+length,x2,y2+length);
+    }
+    else if(x1==x2){
+        int length = y1-y2;
+        printf("%d %d %d %d",x1+length,y1,x2+length,y2);
+    }
+    
+    
+    return 0;
+}
