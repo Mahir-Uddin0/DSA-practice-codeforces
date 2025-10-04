24189859108860d422bba29c5f9b3118a7f53017
Md. Mahir Uddin <157863988+Mahir-Uddin0@users.noreply.github.com>
2025-10-04 16:36:44 +0600

    Update solution.c

--- 1600/A. Mafia/solution.c
+++ 1600/A. Mafia/solution.c
@@ -1,1 +1,22 @@
+#include<stdio.h>
 
+int main(){
+    int n,max=0;
+    long long sum=0;
+    scanf("%d",&n);
+    int a[n];
+    
+    for(int i=0; i<n; i++){
+        scanf("%d",&a[i]);
+        sum+= a[i];
+        if(a[i]>max) max = a[i];
+    }
+    
+    int result = sum/(n-1);
+    if(sum%(n-1)!=0) result++;
+    
+    if(max>result) printf("%d",max);
+    else printf("%d",result);
+    
+    return 0;
+}
