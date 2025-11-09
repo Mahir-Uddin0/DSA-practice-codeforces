98a5baf12bf6add6ef81e137146eb5f95a558761
Md. Mahir Uddin <157863988+Mahir-Uddin0@users.noreply.github.com>
2025-11-09 23:59:04 +0600

    Update solution.c

--- 1400/E. Adjacent XOR/solution.c
+++ 1400/E. Adjacent XOR/solution.c
@@ -1,1 +1,33 @@
+#include <stdio.h>
 
+int main(){
+    int t;
+    scanf("%d ",&t);
+    
+    while(t--){
+        int n,same=1;
+        scanf("%d",&n);
+        int a[n],b[n];
+        
+        for(int i=0; i<n; i++) scanf("%d",&a[i]);
+        for(int i=0; i<n; i++) scanf("%d",&b[i]);
+        
+        if(a[n-1]!=b[n-1]){ printf("NO\n"); continue;}
+        
+        for(int i=0; i<n-1; i++){
+            if(a[i]==b[i]) continue;
+            else if((a[i]^a[i+1])==b[i]) continue;
+            else if((a[i]^b[i+1])==b[i]) continue;
+            else{
+                printf("NO\n");
+                same = 0;
+                break;
+            }
+        }
+        
+        if(same) printf("YES\n");
+        
+    }
+    
+    return 0;
+}
