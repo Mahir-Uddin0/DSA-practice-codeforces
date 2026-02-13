ba235f33d5f1c5a4c40b3ffd728bb98c9bb6213d
Md. Mahir Uddin <157863988+Mahir-Uddin0@users.noreply.github.com>
2026-02-13 23:41:21 +0600

    Update solution.py
    Refactor logic to calculate multiples based on input lists.

--- 1200/C. Production of Snowmen/solution.py
+++ 1200/C. Production of Snowmen/solution.py
@@ -1,1 +1,41 @@
+t = int(input())
 
+while t:
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    c = list(map(int, input().split()))
+    
+    b = b + b
+    c = c + c
+    
+    first_multiple, sec_multiple = 0, 0
+    
+    for j in range(n):
+        found = True
+        for i in range(n):
+            if b[j+i] > a[i]:
+                continue
+            else:
+                found = False
+                break
+            
+        if found:
+            first_multiple += 1
+            
+    
+    for j in range(n):
+        found = True
+        for i in range(n):
+            if c[j+i] > b[i]:
+                continue
+            else:
+                found = False
+                break
+            
+        if found:
+            sec_multiple += 1
+            
+    print(n * first_multiple * sec_multiple)
+    
+    t -= 1
