7a211d47b11020f8ac57315ba78de8485253475e
Md. Mahir Uddin <157863988+Mahir-Uddin0@users.noreply.github.com>
2026-06-11 21:48:03 +0600

    Create solution.py

---
+++ 1300/C1. Seating Arrangement (Easy Version)/solution.py
@@ -0,0 +1,36 @@
+t = int(input())
+
+for _ in range(t):
+    n, x, s = map(int, input().split())
+    guests = input()
+    
+    i_seat, a_hand, e_seat, ans = x, 0, 0, 0
+    
+    for j in range(n):
+        if guests[j] == 'I':
+            if i_seat > 0:
+                i_seat -= 1
+                e_seat += s-1
+                ans += 1
+        elif guests[j] == 'E':
+            if e_seat > 0:
+                e_seat -= 1
+                ans += 1
+            elif a_hand and i_seat:
+                i_seat -= 1
+                a_hand -= 1
+                e_seat += s-1
+                ans += 1
+        elif guests[j] == 'A':
+            if e_seat:
+                e_seat -= 1
+                ans += 1
+                a_hand += 1
+            elif i_seat:
+                i_seat -= 1
+                e_seat += s-1
+                ans += 1
+            else:
+                break
+            
+    print(ans)        
